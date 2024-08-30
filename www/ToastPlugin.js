28a37a1ddcd638585122f5c3c9936a369cdb0b67
IonutVr <86364217+IonutVr@users.noreply.github.com>
2024-08-30 10:36:24 +0100

    Delete www directory

--- www/ToastPlugin.js
+++
@@ -1,9 +0,0 @@
-var exec = require('cordova/exec');
-
-var ToastPlugin = {
-    showToast: function(message, success, error) {
-        exec(success, error, 'ToastPlugin', 'showToast', [message]);
-    }
-};
-
-module.exports = ToastPlugin;
